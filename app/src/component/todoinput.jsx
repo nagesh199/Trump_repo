be7d6fa06cb167f70673a73ff7b5af90430822a9
nagesh199 <99637018+nagesh199@users.noreply.github.com>
2022-09-28 22:14:12 +0530

    add ui

--- app/src/component/todoinput.jsx
+++ app/src/component/todoinput.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import { useDispatch } from "react-redux";
 import { todo } from "../redux/action";
 import { Todoitem } from "./todoitem";
-
+import { Button, TextField } from "@mui/material";
 
 export const Todoinput = ()=>{
     const [text,setText] = useState('');
@@ -10,9 +10,9 @@ export const Todoinput = ()=>{
     const handleClick =()=>{
         dispatch(todo(text))
     }
-    return <div>
-        <input type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
-        <button onClick={handleClick}>Submit</button>
+    return <div style={{marginTop:"30px"}}>
+         <TextField size="small" variant="outlined" type="text" value={text} onChange={(e)=>setText(e.target.value)}/>
+          <Button variant="contained" onClick={handleClick}>Submit</Button>
         <Todoitem/>
     </div>
 }
